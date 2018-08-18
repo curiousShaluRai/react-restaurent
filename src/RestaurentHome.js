3c52c3fd768757525fa0ce54ddef9ccc24acbbba
Shalu Kumari <shaluk@syncroutesoftware.com>
2018-08-18 09:56:41 +0530

    Added homepage

---
+++ src/RestaurentHome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RestaurentHome extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome to my restaurent
+      </div>
+    );
+  }
+}
